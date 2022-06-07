f6f3819f317279520b4a462456659a6986747807
KIWI-LAPTOP\Kiwi <ricardo.martinezc92@gmail.com>
2022-06-07 16:27:02 +0200

    commit

---
+++ Ejercicios/Ejer21.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer21</title>
+</head>
+
+<body>
+    <?php
+    $barreja = shuffle($personatgeSally); //Barrejem valors
+    $dado1 = array(1, 2, 3, 4, 5, 6);
+    $dado2 = array(1, 2, 3, 4, 5, 6);
+
+    $dado1 = shuffle($dado1);
+    var_dump($dado1);
+
+    ?>
+</body>
+
+</html>
